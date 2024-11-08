5c2b3578e43ab400322be7564794c2915fd373a8
정우현 <dngus2063@naver.com>
2024-11-08 11:46:16 +0900

    bootstrap,router initialize
    bootstrap,router initialize

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+<<<<<<< Updated upstream
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 createApp(App).mount('#app')
+=======
+import router from './router'
+import store from './store'
+
+//부트스트랩
+import BootstrapVue3 from 'bootstrap-vue-3'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
+createApp(App).use(store).use(router).use(BootstrapVue3).mount('#app')
+>>>>>>> Stashed changes
